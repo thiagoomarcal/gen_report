5356cc4d04751bdf74f39a04c8d494612cfecc90
Thiago Oliveira Marçal <thiagoomarcal@protonmail.com>
2022-06-08 19:30:02 -0300

    Trilha Elixir Ignite - Desafio 01 - Gerando relatórios

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,82 @@
 defmodule GenReport do
+  alias GenReport.Parser
 
-  def build do
-    # TO DO
+  @names [
+    "diego",
+    "vinicius",
+    "cleiton",
+    "daniele",
+    "mayk",
+    "danilo",
+    "giuliano",
+    "jakeliny",
+    "joseph",
+    "rafael"
+  ]
+
+  @months [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro"
+  ]
+
+  @years [
+    2016,
+    2017,
+    2018,
+    2019,
+    2020
+  ]
+
+  def build() do
+    {:error, "Insira o nome de um arquivo"}
+  end
+
+  def build(filename) do
+    result =
+      filename
+      |> Parser.parse_file()
+      |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
+
+    {:ok, result}
+  end
+
+  defp sum_values([name, hours, _day, month, year], %{
+         "all_hours" => all_hours,
+         "hours_per_month" => hours_per_month,
+         "hours_per_year" => hours_per_year
+       }) do
+    all_hours = Map.put(all_hours, name, all_hours[name] + hours)
+    hours_per_month = put_in(hours_per_month, [name, month], hours_per_month[name][month] + hours)
+    hours_per_year = put_in(hours_per_year, [name, year], hours_per_year[name][year] + hours)
+    build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp report_acc() do
+    all_hours = Enum.into(@names, %{}, &{&1, 0})
+    hours_per_month = Enum.into(@names, %{}, &build_map(&1, @months))
+    hours_per_year = Enum.into(@names, %{}, &build_map(&1, @years))
+    build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp build_map(key, values) do
+    {key, Enum.into(values, %{}, &{&1, 0})}
+  end
+
+  defp build_report(all_hours, hours_per_month, hours_per_year) do
+    %{
+      "all_hours" => all_hours,
+      "hours_per_month" => hours_per_month,
+      "hours_per_year" => hours_per_year
+    }
   end
 end
